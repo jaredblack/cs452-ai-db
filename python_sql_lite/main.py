65490438d9d70e84e8abee9a939210d5360f7c16
bjfoutz00 <bjfoutz00@gmail.com>
2023-10-06 11:19:08 -0600

    Slightly modified main

--- python_sql_lite/main.py
+++ python_sql_lite/main.py
@@ -8,15 +8,16 @@ from db import create_connection
 
 DATABASE = "./horse.db"
 
-def main(conn, question):
+def main(conn, prompt):
     # Load your API key from an environment variable or secret management service
-    openai.api_key = os.getenv("OPENAI_KEY")
-    print(f"Question: {question}")
+    openai.api_key = "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"
+    # openai.api_key = os.getenv("OPENAI_KEY")
+    print(f"Prompt: {prompt}")
 
     prompt = f"""
     
     Given the following SQL Schema:{get_schema()}
-    Write a SQL query to answer this question: {question}
+    Write a valid SQLite query to answer this prompt: {prompt}
     
     """
 
@@ -37,9 +38,9 @@ def main(conn, question):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--query", type=str, default="natural language query")
+    parser.add_argument("--query", type=str, default="Who is the best horse?")
     args = parser.parse_args()
     conn = create_connection(DATABASE)
 
-    main(conn, question=args.query)
+    main(conn, prompt=args.query)
 
